f6c9d18befef72e776b558922f9711eba33827c1
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-03-26 19:05:49 +0530

    code: updating just O+ part

--- scripts/world-map.js
+++ scripts/world-map.js
@@ -32,6 +32,24 @@ document.addEventListener('DOMContentLoaded', async function () {
         if (slider_value == 2) {
             world_map_or_horizontal_bar_graph = 2;
         }
+        else if (slider_value == 3){
+            world_map_or_horizontal_bar_graph = 3; 
+        }
+        else if ( slider_value == 4){
+            world_map_or_horizontal_bar_graph = 4; 
+        }
+        else if ( slider_value == 5 ){
+            world_map_or_horizontal_bar_graph = 5; 
+        }
+        else if ( slider_value == 6 ){
+            world_map_or_horizontal_bar_graph = 6; 
+        }
+        else if ( slider_value == 7 ){
+            world_map_or_horizontal_bar_graph = 7; 
+        }
+        else if(slider_value == 8){
+            world_map_or_horizontal_bar_graph = 8;
+        }
         let temporary_check_int = 0; 
         console.log(world_map_or_horizontal_bar_graph)
         // Load the GeoJSON data
@@ -85,10 +103,13 @@ document.addEventListener('DOMContentLoaded', async function () {
                         // console.log(d.properties.name);
                         if (ExtractedData[j][0] == d.properties.name) {
                             temporary_check_int = temporary_check_int  + 1; 
-                            
-                            var individualCountryExportData = ExtractedData[j][1];
+                            // console.log(ExtractedData);
+                            var individualCountryExportData = ExtractedData[j][world_map_or_horizontal_bar_graph];
                             result = individualCountryExportData.replace('%', '');
-                            console.log(result);
+                            // console.log(result);
+                            console.log(slider_value)
+                            console.log(world_map_or_horizontal_bar_graph);
+                            // console.log(d.properties.name);
 
                             if (result > 50) {
                                 // console.log(individualCountryExportData);
@@ -101,7 +122,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                             else if (result > 30) {
                                 return "#C2A5CF";
                             }
-                            else if (result > 10) {
+                            else if (result > 20) {
                                 return "pink";
                             }
                             else {
@@ -116,18 +137,46 @@ document.addEventListener('DOMContentLoaded', async function () {
                 })
                 .attr("stroke", "white")
                 .attr("stroke-width", 0.5)
-                .on("mouseover", function (d) {
-                    d3.select(this)
-                        .attr("stroke-width", 2)
+                .on("mouseover", function (event, d, i) {
+                    let point = this.getAttribute('class');
+                    console.log(point);
+    
+                    console.log(this);
+                    // d3.select(this).style("fill", "orange");
+                    // Calculate the position of the tooltip relative to the SVG container
+                    const tooltipX = event.offsetX  + 100;
+                    const tooltipY = event.offsetY + 100;
+    
+                    tooltip.html(` ${d.properties.name}`)
+                        .style("left", tooltipX + "px")
+                        .style("top", tooltipY + "px")
+                        .style("opacity", 1)
+                        .style('visibility', "visible");
+    
                 })
-                .on("mouseout", function (d) {
-                    d3.select(this)
-                        .attr("stroke-width", 0.5)
-                })
+                .on("mouseout", (d) => {
+                    tooltip.style('visibility', "hidden");
+                });
                 // console.log(temporary_check_int);
         });
        
     }
+
+    // Adding Tool Tip to the Graphs
+    const tooltip = d3.select("#world-map").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 1)
+    .style("position", "absolute")
+    .style("width", function(d) {
+        // Adjust width dynamically based on content
+        // For example:
+        return 100 + "px"; // Adjust multiplier as needed
+    })
+    .style("height", function(d) {
+        // Adjust width dynamically based on content
+        // For example:
+        return 50 + "px"; // Adjust multiplier as needed
+    });
 
 
 
