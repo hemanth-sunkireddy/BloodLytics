44895aa53e027d482363612507757ac72c9c981b
bharath-gajawada <bharathgajawada2004@gmail.com>
2024-03-26 20:04:19 +0530

    updated

--- scripts/india-map.js
+++ scripts/india-map.js
@@ -1,108 +1,134 @@
+let margin = {top: 10, right: 20, bottom: 10, left: 20};
+let width = 1200 - margin.left - margin.right;
+let height = 700 - margin.top - margin.bottom;
 
-    let margin = {top: 10, right: 20, bottom: 10, left: 450};
-    let width = 1200 - margin.left - margin.right;
-    let height = 700 - margin.top - margin.bottom;
-    
-    let svg = d3.select("#postContents").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-          
-    svg.append("rect")
-      .attr("width",width)
-      .attr("height",height)
-      .style("fill","green")
-      .style("opacity", 0);
-        
-    let div = d3.select("body").append("div")	
-      .attr("class", "tooltip")				
-      .style("opacity", 0);
-    
-    Promise.all([
-      d3.json("Maps_JSON/india-states.json"),
-      d3.csv("Data_Sets/blood_data.csv")
-    ]).then(function(data){
-      let geoJsonData = data[0];
-      let bloodData = data[1];
-      let projection = d3.geoMercator();
-      projection.fitHeight(height-100,geoJsonData);
-      let pathGenerator = d3.geoPath().projection(projection);
+let svg = d3.select("#postContents").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
       
-      let india = svg.append('g');     
-      india.selectAll('path')
-        .data(geoJsonData.features)
-        .enter()
-        .append('path')
-          .attr('d', pathGenerator)
-          .style("stroke", "black")
-          .on("mouseover", function(d) {
-            div.style("opacity", .9);		
-            div.html(d.state + "</br>" 
-            + "Donations: " + d.donations + "</br>" 
-            + "Requirements: " + d.requirements + "</br>")
-            
-            .style("left", (d3.event.pageX) + "px")		
-            .style("top", (d3.event.pageY - 28) + "px");
-            
-            let barData1 = [
-              { type: "Donations", value: d.donations },
-            ];
+svg.append("rect")
+  .attr("width",width)
+  .attr("height",height)
+  .style("fill","green")
+  .style("opacity", 0);
 
-            let barData2 = [
-              { type: "Requirements", value: d.requirements }
-            ];
-            
-            let tooltipSvg = div.append("svg")
-              .attr("width", 150)
-              .attr("height", 100);
-            
-            let xScale = d3.scaleBand()
-              .domain(["Donations", "Requirements"])
-              .range([0, 100])
-              .padding(0.1);
-            
-            let yScale = d3.scaleLinear()
-              .domain([0, Math.max(d.donations, d.requirements)])
-              .nice()
-              .range([80, 0]);
-            
-            let bars1 = tooltipSvg.selectAll(".bar1")
-              .data(barData1)
-              .enter()
-              .append("rect")
-                .attr("class", "bar-donations")
-                .attr("x", d => xScale(d.type))
-                .attr("y", d => yScale(d.value))
-                .attr("width", xScale.bandwidth())
-                .attr("height", d => 80 - yScale(d.value));
+let div = d3.select("body").append("div")  
+  .attr("class", "tooltip")        
+  .style("opacity", 0);
 
-            let bars2 = tooltipSvg.selectAll(".bar2")
-              .data(barData2)
-              .enter()
-              .append("rect")
-                .attr("class", "bar-requirements")
-                .attr("x", d => xScale(d.type))
-                .attr("y", d => yScale(d.value))
-                .attr("width", xScale.bandwidth())
-                .attr("height", d => 80 - yScale(d.value));
-            
-          })					
-          .on("mouseout", function(d) {		
-            div.style("opacity", 0);
-            div.selectAll("svg").remove();
-          });
-      
-      let colorScale = d3.scaleLinear()
-        .domain([0.5, 1, 2])
-        .range(['red', 'lightblue', 'blue']);
+// Load all CSV data
+Promise.all([
+  d3.json("Maps_JSON/india-states.json"),
+  d3.csv("Data_Sets/FamilyBloodDonor.csv"),
+  d3.csv("Data_Sets/ReplacementBloodDonor.csv"),
+  d3.csv("Data_Sets/VoluntaryBloodDonor.csv"),
+  d3.csv("Data_Sets/TotalBloodDonor.csv")
+]).then(function(data){
+  console.log("Data loaded successfully:", data);
+  let geoJsonData = data[0];
+  let bloodData = {
+    'Family': data[1],
+    'Replacement': data[2],
+    'Voluntary': data[3],
+    'Total': data[4]
+  };
+  console.log("Blood data:", bloodData);
+  let projection = d3.geoMercator();
+  projection.fitHeight(height-100,geoJsonData);
+  let pathGenerator = d3.geoPath().projection(projection);
+  
+  let india = svg.append('g');     
+  india.selectAll('path')
+    .data(geoJsonData.features)
+    .enter()
+    .append('path')
+      .attr('d', pathGenerator)
+      .style("stroke", "black")
+      .on("mouseover", function(d) {
+        div.style("opacity", .9);     
+        div.html(d.state + "</br>" 
+        + "MaleDonors: " + d.MaleDonors + "</br>" 
+        + "FemaleDonors: " + d.FemaleDonors + "</br>")
         
-      india.selectAll('path')
-        .data(bloodData)
-        .style("fill", function(d) {
-          let ratio = d.donations / d.requirements;
-          return colorScale(ratio);
-        });
-              
-    })
+        .style("left", (d3.event.pageX) + "px")      
+        .style("top", (d3.event.pageY - 28) + "px");
+        
+        // Bar chart data for tooltip
+        let barData1 = [
+          { type: "Male", value: d.MaleDonors },
+          { type: "Female", value: d.FemaleDonors }
+        ];
+
+        let tooltipSvg = div.append("svg")
+          .attr("width", 150)
+          .attr("height", 100);
+        
+        let xScale = d3.scaleBand()
+          .domain(["Male", "Female"])
+          .range([0, 100])
+          .padding(0.1);
+        
+        let yScale = d3.scaleLinear()
+          .domain([0, Math.max(d.MaleDonors, d.FemaleDonors)])
+          .nice()
+          .range([80, 0]);
+        
+        let bars1 = tooltipSvg.selectAll(".bar1")
+          .data(barData1)
+          .enter()
+          .append("rect")
+            .attr("class", "bar-donations")
+            .attr("x", d => xScale(d.type))
+            .attr("y", d => yScale(d.value))
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => 80 - yScale(d.value));
+        
+      })         
+      .on("mouseout", function(d) {     
+        div.style("opacity", 0);
+        div.selectAll("svg").remove();
+      });
+  
+  // Dropdown menu for selecting data type
+  let dropdown = d3.select("#postContents")
+    .append("select")
+    .attr("id", "dropdown")
+    .style("position", "absolute")
+    .style("top", "20px")
+    .style("left", "20px");
+
+  dropdown.selectAll("option")
+    .data(Object.keys(bloodData))
+    .enter().append("option")
+    .attr("value", d => d)
+    .text(d => d);
+
+  dropdown.on("change", function() {
+    let selectedData = bloodData[this.value];
+    updateChoropleth(selectedData);
+  });
+
+  // Initial update
+  updateChoropleth(bloodData['Total']);
+
+  function updateChoropleth(data) {
+    console.log("Updating choropleth with data:", data);
+    let colorScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => parseInt(d.TotalDonors))])
+      .range(['lightyellow', 'orange']);
+
+    india.selectAll('path')
+      .data(geoJsonData.features)
+      .style("fill", d => {
+        let stateData = data.find(entry => entry.state === d.id);
+        if (stateData) {
+          return colorScale(parseInt(stateData.TotalDonors));
+        } else {
+          return "grey"; // Fallback color for missing data
+        }
+      });
+  }
+});
