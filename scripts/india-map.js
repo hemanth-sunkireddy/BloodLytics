be543ba540ddf338c7d7a8cdd1243a69b43566c1
bharath-gajawada <bharathgajawada2004@gmail.com>
2024-03-28 17:04:57 +0530

    india choropleth

--- scripts/india-map.js
+++ scripts/india-map.js
@@ -1,20 +1,27 @@
-// Define margins for the visualization
-let margin = { top: 10, right: 20, bottom: 10, left: 20 };
-let width = 1200 - margin.left - margin.right;
-let height = 700 - margin.top - margin.bottom;
+// Define margins and dimensions for the visualization
+let margin = { top: 20, right: 50, bottom: 50, left: 100 };
+let svgWidth = 1200;
+let svgHeight = 1100;
+let mapHeight = 600;
+let chartHeight = 400;
+let chartWidth = svgWidth - margin.left - margin.right;
 
-// Append an SVG element to the DOM for the map
+// Append an SVG element to the DOM
 let svg = d3.select("#postContents").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom);
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
 // Append a group for the map
 let map = svg.append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Append a group for the bar graph
+// Append a group for the choropleth
+let choropleth = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Append a group for the bar chart
 let barGraph = svg.append("g")
-  .attr("transform", "translate(" + margin.left + "," + (height - 50) + ")");
+  .attr("transform", `translate(${svgWidth / 2}, ${mapHeight + margin.top + 30})`);
 
 // Append a div for the tooltip
 let div = d3.select("body").append("div")
@@ -43,7 +50,7 @@ Promise.all([
 
   // Define projection for the map
   let projection = d3.geoMercator()
-    .fitHeight(height, geoJsonData);
+    .fitHeight(mapHeight, geoJsonData);
   let pathGenerator = d3.geoPath().projection(projection);
 
   // Append a group for India map
@@ -52,7 +59,7 @@ Promise.all([
   // Append a group for the legend
   let legend = svg.append("g")
     .attr("class", "legend")
-    .attr("transform", "translate(" + (width - 150) + "," + (height - 200) + ")");
+    .attr("transform", `translate(${svgWidth - 150 - margin.right}, ${margin.top})`);
 
   // Function to update legend based on selected data
   function updateLegend(value) {
@@ -111,12 +118,10 @@ Promise.all([
   // Call updateLegend initially
   updateLegend('Family');
 
-
   // Function to update choropleth based on selected data
   function updateChoropleth(value) {
     let data = bloodData[value]
     console.log("Updating choropleth with data:", data);
-
 
     // Update map colors based on data
     india.selectAll('path')
@@ -155,11 +160,23 @@ Promise.all([
         // Mouseover event handler for tooltip
         div.style("opacity", .9);
         let stateData = data.find(entry => entry.state === d.id);
+        if (value === "Total")
+        {
         div.html(stateData.state + "</br>"
+          + "TotalDonors: " + stateData.TotalDonors)
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+        }
+        else
+        {
+          div.html(stateData.state + "</br>"
           + "MaleDonors: " + stateData.MaleDonors + "</br>"
           + "FemaleDonors: " + stateData.FemaleDonors + "</br>")
           .style("left", (d3.event.pageX) + "px")
           .style("top", (d3.event.pageY - 28) + "px");
+        }
+        // Highlight corresponding bar in the bar chart
+        highlightStateInBarChart(stateData.state);
       })
       .on("mouseout", function (d) {
         // Mouseout event handler for hiding tooltip
@@ -187,59 +204,183 @@ Promise.all([
 
   // Dropdown change event handler
   dropdown.on("change", function () {
-    let selectedData = bloodData[this.value];
+    // let selectedData = bloodData[this.value];
     console.log("type: ", this.value);
     updateChoropleth(this.value);
-    updateBarGraph(selectedData);
+    updateBarGraph(this.value);
   });
 
   // Initial update of choropleth
   updateChoropleth('Family');
 
-  // Function to update bar graph
-  function updateBarGraph(data) {
+  // Function to update horizontal bar graph
+  function updateBarGraph(value) {
     // Remove existing bar graph
     barGraph.selectAll("*").remove();
+    let data = bloodData[value];
+    // Create data for the horizontal bar chart
+    console.log("bar:", data);
+    let barData = data.map(d => {
+      let newData = {
+        state: d.state
+      };
 
-    // Aggregate data for bar graph
-    let maleDonors = d3.sum(data, d => parseInt(d.MaleDonors));
-    let femaleDonors = d3.sum(data, d => parseInt(d.FemaleDonors));
+      if (value === "Total") 
+      {
+        newData.TotalDonors = parseInt(d.TotalDonors);
+      } 
+      else 
+      {
+        newData.MaleDonors = parseInt(d.MaleDonors);
+        newData.FemaleDonors = parseInt(d.FemaleDonors);
+      }
 
-    // Create data for the bar graph
-    let barData = [
-      { type: "Male", value: maleDonors },
-      { type: "Female", value: femaleDonors }
-    ];
+      return newData;
+    });
+
 
     // Define scales for the bar graph
-    let xScale = d3.scaleBand()
-      .domain(barData.map(d => d.type))
-      .range([margin.left, width - margin.right])
+    let xScale;
+  if (value === "Total") {
+    xScale = d3.scaleLinear()
+      .domain([0, 2 * d3.max(barData, d => d.TotalDonors)])
+      .range([0, chartWidth]);
+  } else {
+    xScale = d3.scaleLinear()
+      .domain([0, 2 * d3.max(barData, d => d.MaleDonors + d.FemaleDonors)])
+      .range([0, chartWidth]);
+  }
+    let yScale = d3.scaleBand()
+      .domain(barData.map(d => d.state))
+      .range([0, chartHeight])
       .padding(0.1);
 
-    let yScale = d3.scaleLinear()
-      .domain([0, d3.max(barData, d => d.value)])
-      .nice()
-      .range([50, 0]);
-
     // Append bars to the bar graph
-    barGraph.selectAll(".bar")
-      .data(barData)
-      .join("rect")
-      .attr("class", "bar")
-      .attr("x", d => xScale(d.type))
-      .attr("y", d => yScale(d.value))
-      .attr("width", xScale.bandwidth())
-      .attr("height", d => 50 - yScale(d.value));
+    barGraph.selectAll(".barGroup")
+    .data(barData)
+    .enter().append("g")
+    .attr("class", "barGroup")
+    .attr("transform", d => `translate(0, ${yScale(d.state)})`)
+    .each(function (d) {
+      if (value === "Total") {
+        d3.select(this).append("rect")
+          .attr("class", "bar")
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr("width", xScale(d.TotalDonors))
+          .attr("height", yScale.bandwidth())
+          .style("fill", "steelblue");
+      } else {
+        d3.select(this).append("rect")
+          .attr("class", "maleBar")
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr("width", d => xScale(d.MaleDonors))
+          .attr("height", yScale.bandwidth() / 2)
+          .style("fill", "steelblue");
 
-    // Add labels
-    barGraph.selectAll(".bar-label")
-      .data(barData)
-      .join("text")
-      .attr("class", "bar-label")
-      .attr("x", d => xScale(d.type) + xScale.bandwidth() / 2)
-      .attr("y", d => yScale(d.value) - 5)
-      .attr("text-anchor", "middle")
-      .text(d => d.value);
+        d3.select(this).append("rect")
+          .attr("class", "femaleBar")
+          .attr("x", 0)
+          .attr("y", yScale.bandwidth() / 2)
+          .attr("width", d => xScale(d.FemaleDonors))
+          .attr("height", yScale.bandwidth() / 2)
+          .style("fill", "lightblue");
+      }
+    })
+      .on("mouseover", function (d) {
+        // Mouseover event handler for tooltip
+        div.style("opacity", .9);
+        if (value === "Total") {
+          div.html("State: " + d.state + "<br/>"
+            + "Total Donors: " + d.TotalDonors)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+        }
+        else {
+          div.html("State: " + d.state + "<br/>"
+            + "Male Donors: " + d.MaleDonors + "<br/>"
+            + "Female Donors: " + d.FemaleDonors)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+        }
+        // Highlight corresponding state in the choropleth map
+        highlightBarInChoropleth(d.state);
+      })
+      .on("mouseout", function (d) {
+        // Mouseout event handler for hiding tooltip
+        div.style("opacity", 0);
+      });
+
+    // Add y-axis with state names
+    barGraph.append("g")
+      .attr("class", "y-axis")
+      .call(d3.axisLeft(yScale))
+      .selectAll("text")
+      .attr("dy", "0.35em")
+      .attr("transform", "translate(-10, 0)")
+      .style("text-anchor", "end");
+
+    // Add x-axis with values
+    barGraph.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(d3.axisBottom(xScale).ticks(5));
   }
+
+  // Function to handle hovering over a state in the choropleth map
+  function highlightStateInBarChart(stateId) {
+    // Remove any existing highlighting
+    barGraph.selectAll(".bar")
+      .classed("highlighted", false);
+
+    // Highlight the corresponding bar in the bar chart
+    barGraph.selectAll(".bar")
+      .filter(d => d.state === stateId)
+      .classed("highlighted", true);
+  }
+
+  // Function to handle hovering over a bar in the bar chart
+  function highlightBarInChoropleth(stateId) {
+    // Remove any existing highlighting
+    india.selectAll('path')
+      .style("stroke-width", "1px");
+
+    // Highlight the corresponding state in the choropleth map
+    india.selectAll('path')
+      .filter(d => d.id === stateId)
+      .style("stroke-width", "3px");
+  }
+
+  // Mouseover event handler for states in choropleth map
+  india.selectAll('path')
+    .on("mouseover", function (d) {
+      let stateId = d.id;
+      highlightStateInBarChart(stateId);
+      highlightBarInChoropleth(stateId);
+    })
+    .on("mouseout", function () {
+      // Remove highlighting on mouseout
+      barGraph.selectAll(".bar")
+        .classed("highlighted", false);
+
+      india.selectAll('path')
+        .style("stroke-width", "1px");
+    });
+
+  // Mouseover event handler for bars in the bar chart
+  barGraph.selectAll('.bar')
+    .on("mouseover", function (d) {
+      let stateId = d.state;
+      highlightStateInBarChart(stateId);
+      highlightBarInChoropleth(stateId);
+    })
+    .on("mouseout", function () {
+      // Remove highlighting on mouseout
+      barGraph.selectAll(".bar")
+        .classed("highlighted", false);
+
+      india.selectAll('path')
+        .style("stroke-width", "1px");
+    });
 });
